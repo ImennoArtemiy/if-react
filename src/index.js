352e7cf937a92cec18cdbe3f6a2bca41a4b1f9e5
ImennoArtemiy <dakerdeath014@gmail.com>
2022-04-06 22:01:13 +0300

    used react router to navigate hotel info, added footer component

--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './views/App';
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import HotelPage from "./views/HotelPage/HotelPage";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App/>
+      <BrowserRouter>
+          <Routes>
+              <Route path="/" element={<App />} />
+              <Route path='hotel/:id' element={ <HotelPage />} />
+          </Routes>
+      </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
